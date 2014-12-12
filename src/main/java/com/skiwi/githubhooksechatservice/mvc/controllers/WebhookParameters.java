d638b7bd18d9287ceb19a2e94b826b66d0b1dca9
Simon Forsberg <zomis2k@hotmail.com>
2014-12-12 20:29:38 +0100

    added WebhookParameters and ChatMessage classes

---
+++ src/main/java/com/skiwi/githubhooksechatservice/mvc/controllers/WebhookParameters.java
@@ -0,0 +1,15 @@
+package com.skiwi.githubhooksechatservice.mvc.controllers;
+
+public class WebhookParameters {
+	
+	private String room;
+	
+	public String getRoom() {
+		return room;
+	}
+	
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	
+}
