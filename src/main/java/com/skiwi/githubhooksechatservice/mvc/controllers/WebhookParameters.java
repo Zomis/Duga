0262b287d4eda396776c579c731ec5bc1fbb846d
Simon Forsberg <zomis2k@hotmail.com>
2014-12-17 02:20:46 +0100

    added possibility to disable posting messages and only logging by using ?post=false

--- src/main/java/com/skiwi/githubhooksechatservice/mvc/controllers/WebhookParameters.java
+++ src/main/java/com/skiwi/githubhooksechatservice/mvc/controllers/WebhookParameters.java
@@ -5,6 +5,7 @@ import com.skiwi.githubhooksechatservice.mvc.configuration.Configuration;
 public class WebhookParameters {
 	
 	private String room;
+	private Boolean post;
 	
 	public String getRoom() {
 		return room;
@@ -18,6 +19,14 @@ public class WebhookParameters {
 		if (room == null) {
 			room = configuration.getRoomId();
 		}
+	}
+	
+	public boolean getPost() {
+		return post == null ? true : post;
+	}
+	
+	public void setPost(Boolean post) {
+		this.post = post;
 	}
 	
 }
