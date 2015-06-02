2372008de18675b02516fa2213c5a5a3ead9b882
Nick <newbiedoodle@gmail.com>
2015-06-02 19:24:33 -0400

    Clarifying comments and implementation

--- src/main/java/com/skiwi/githubhooksechatservice/mvc/beans/tasks/CommentsScanTask.java
+++ src/main/java/com/skiwi/githubhooksechatservice/mvc/beans/tasks/CommentsScanTask.java
@@ -31,7 +31,11 @@ public class CommentsScanTask implements Runnable {
 
 	private ChatBot chatBot;
 	
-	private Pattern interestingComment = Pattern.compile("[*`_]{,2}code[*`_]{,2}\s*[*`_]{,2}review[*`_]{,2}", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	/**
+	 * If any substring of a comment matches this pattern, it's probably interesting.<br>
+	 * No need for case-insensitivity since the comment is already lower-cased by the time it's being compared.
+	 */
+	private Pattern interestingComment = Pattern.compile("[*`_]{,2}code[*`_]{,2}\s*[*`_]{,2}review[*`_]{,2}");
 	
 	public CommentsScanTask(StackExchangeAPIBean stackAPI, ChatBot chatBot) {
 		this.stackAPI = stackAPI;
@@ -39,10 +43,11 @@ public class CommentsScanTask implements Runnable {
 	}
 
 	private boolean isInterestingComment(StackExchangeComment comment) {
-		String commentText = comment.getBodyMarkdown().toLowerCase();
+		String commentText = comment.getBodyMarkdown().toLowerCase(Locale.ENGLISH);
 		return interestingComment.matcher(commentText).find(0);
 		// We're using `find()` instead of `matches()` because it can occur anywhere and we don't wanna reject if we can't match the whole comment
 		// See https://docs.oracle.com/javase/8/docs/api/java/util/regex/Matcher.html#matches-- and #find-int- for more information
+		// (and we're using find(0) to make sure it searches from the start, not somewhere in the middle)
 	}
 
 	@Override
