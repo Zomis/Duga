2c724d3fb5ef6290154a2e786f6749164e2ad2b7
Nick <newbiedoodle@gmail.com>
2015-06-02 20:58:50 -0400

    Removed comments so it's just code, added some tests

--- src/test/java/com/skiwi/githubhooksechatservice/mvc/beans/tasks/CommentsScanTaskTest.java
+++ src/test/java/com/skiwi/githubhooksechatservice/mvc/beans/tasks/CommentsScanTaskTest.java
@@ -1,13 +1,31 @@
 import static org.junit.Assert.*;
 
 public class CommentsScanTaskTest {
-  private Pattern interestingComment = Pattern.compile("[*`_]{,2}code[*`_]{,2}\s*[*`_]{,2}review[*`_]{,2}");
+	private Pattern interestingComment = Pattern.compile("[*`_]{,3}code[*`_]{,3}\s*[*`_]{,3}review[*`_]{,3}");
 	
-	private boolean isInterestingComment(String comment) {
+	private static boolean isInterestingComment(String comment) {
 		String commentText = comment.toLowerCase(Locale.ENGLISH);
 		return interestingComment.matcher(commentText).find(0);
-		// We're using `find()` instead of `matches()` because it can occur anywhere and we don't wanna reject if we can't match the whole comment
-		// See https://docs.oracle.com/javase/8/docs/api/java/util/regex/Matcher.html#matches-- and #find-int- for more information
-		// (and we're using find(0) to make sure it searches from the start, not somewhere in the middle)
+	}
+	
+	public void test() throws Exception {
+		String[] shouldMatch = {
+			"This is a demo comment containing the text Code Review.",
+			"This is another with a formatted _Code_ **Review**",
+			"This isn't valid Markdown, but *_Code Review*_'s Duga don't care.",
+			"Then, of course, CodeReview and coderevIEW and COde       REView should also all be matched",
+			"This is a comment about how you need a code review!"
+		};
+		String[] shouldntMatch = {
+			"This is a comment without the C/R words in it.",
+			"This is Co*de Review* with formatting in the middle, which isn't matched by design",
+			"[Code](http://google.com) Review definitely should not though.",
+			"This is a comment about apologizing for code and review.",
+			"This is testing that that great site, Code Review, is matched"
+		}
+		for (String match : shouldMatch)
+			assertTrue(match + " should be interesting, but isn't!", isInterestingComment(match));
+		for (String match : shouldMatch)
+			assertFalse(match + " shouldn't be interesting, but is!", isInterestingComment(match));
 	}
 }
