a7e3fa7ae2b13a36761a9bc7b5b544b39416543f
Simon Forsberg <zomis2k@hotmail.com>
2015-06-06 23:12:18 +0200

    only creating default users if no others exist

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -12,17 +12,18 @@ class BootStrap {
 
         switch (Environment.current) {
             case Environment.DEVELOPMENT:
+                def users = User.list()
+                if (users.isEmpty()) {
+                    def user = new User(username: 'user', password: 'user', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
+                    def admin = new User(username: 'admin', password: 'admin', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
 
-                def user = new User(username: 'user', password: 'user', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
-                def admin = new User(username: 'admin', password: 'admin', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
+                    def roleUser = new Authority(authority: 'ROLE_USER').save(failOnError: true)
+                    def roleAdmin = new Authority(authority: 'ROLE_ADMIN').save(failOnError: true)
 
-                def roleUser = new Authority(authority: 'ROLE_USER').save(failOnError: true)
-                def roleAdmin = new Authority(authority: 'ROLE_ADMIN').save(failOnError: true)
-
-                UserAuthority.create(user, roleUser, true)
-                UserAuthority.create(admin, roleUser, true)
-                UserAuthority.create(admin, roleAdmin, true)
-
+                    UserAuthority.create(user, roleUser, true)
+                    UserAuthority.create(admin, roleUser, true)
+                    UserAuthority.create(admin, roleAdmin, true)
+                }
                 break
             case Environment.PRODUCTION:
 
