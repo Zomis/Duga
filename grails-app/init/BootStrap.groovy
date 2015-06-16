851c3c04d91729becf42df2fe03c4d1e34209ecc
Simon Forsberg <zomis2k@hotmail.com>
2015-06-16 21:38:47 +0200

    always creates default users and authorities if there is are no users on startup

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,5 @@
 import grails.util.Environment
 import net.zomis.duga.Authority
-import net.zomis.duga.TaskData
 import net.zomis.duga.User
 import net.zomis.duga.UserAuthority
 
@@ -9,27 +8,20 @@ class BootStrap {
     def tasks
 
     def init = { servletContext ->
-
-        switch (Environment.current) {
-            case Environment.DEVELOPMENT:
-                def users = User.list()
-                if (users.isEmpty()) {
-                    def user = new User(username: 'user', password: 'user', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
-                    def admin = new User(username: 'admin', password: 'admin', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
-
-                    def roleUser = new Authority(authority: 'ROLE_USER').save(failOnError: true)
-                    def roleAdmin = new Authority(authority: 'ROLE_ADMIN').save(failOnError: true)
-
-                    UserAuthority.create(user, roleUser, true)
-                    UserAuthority.create(admin, roleUser, true)
-                    UserAuthority.create(admin, roleAdmin, true)
-                }
-                break
-            case Environment.PRODUCTION:
-
-                break
+        def users = User.list()
+        if (users.isEmpty()) {
+            def roleUser = new Authority(authority: 'ROLE_USER').save(failOnError: true)
+            def roleAdmin = new Authority(authority: 'ROLE_ADMIN').save(failOnError: true)
+            def user = new User(username: 'user', password: 'user', enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
+            String adminPassword = 'admin' + Math.random()
+            if (Environment.current == Environment.DEVELOPMENT) {
+                adminPassword = 'admin'
+            }
+            def admin = new User(username: 'admin', password: adminPassword, enabled: true, accountExpired: false, accountLocked: false, credentialsExpired: false ).save(failOnError: true)
+            UserAuthority.create(user, roleUser, true)
+            UserAuthority.create(admin, roleUser, true)
+            UserAuthority.create(admin, roleAdmin, true)
         }
-
         tasks.initOnce()
     }
     def destroy = {
