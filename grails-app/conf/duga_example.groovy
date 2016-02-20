4bb5bbe4dc5a2b127dc298789dfc59e30778fca8
Simon Forsberg <zomis2k@hotmail.com>
2016-02-20 21:13:45 +0100

    Add duga_example.groovy

---
+++ grails-app/conf/duga_example.groovy
@@ -0,0 +1,14 @@
+botName = 'Greger'
+rootUrl = 'http://stackexchange.com'
+email = 'your@email.com'
+password = 'SE account password'
+
+stackAPI = 'Stack Exchange API Key'
+githubAPI = 'Github API Key'
+adminDefaultPass = 'adminpassword'
+commandPrefix = '@BotName do '
+
+dataSource {
+    username = 'duga'
+    password = 'duga'
+}
