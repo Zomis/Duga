b067ff06362fd5ebbf607fee058fa4893fcd426d
Simon Forsberg <zomis2k@hotmail.com>
2016-02-21 02:09:32 +0100

    Make listen task use the general task from duga-core

--- src/main/groovy/net/zomis/duga/tasks/ListenTask.groovy
+++ src/main/groovy/net/zomis/duga/tasks/ListenTask.groovy
@@ -1,10 +1,6 @@
 package net.zomis.duga.tasks
 
-import com.gistlabs.mechanize.Resource
-import com.gistlabs.mechanize.document.json.JsonDocument
-import com.gistlabs.mechanize.document.json.node.JsonNode
 import com.gistlabs.mechanize.impl.MechanizeAgent
-import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
 import groovy.transform.TimedInterrupt
 import net.zomis.duga.ChatCommands
@@ -12,6 +8,7 @@ import net.zomis.duga.DugaBotService
 import net.zomis.duga.DugaChatListener
 import net.zomis.duga.chat.listen.ChatMessageIncoming
 import net.zomis.duga.chat.BotRoom
+import net.zomis.duga.chat.listen.StackExchangeFetch
 import org.codehaus.groovy.ast.expr.Expression
 import org.codehaus.groovy.ast.expr.VariableExpression
 import org.codehaus.groovy.control.CompilerConfiguration
@@ -24,8 +21,6 @@ import static org.codehaus.groovy.syntax.Types.*
 
 class ListenTask implements Runnable {
 
-    private static final int NUM_MESSAGES = 10
-
     private final String commandPrefix
     private final DugaBotService bot
     private final String room
@@ -33,9 +28,8 @@ class ListenTask implements Runnable {
     private final DugaChatListener bean
     private final ChatCommands handler
     private final GroovyShell groovyShell
-    private long lastHandledId
-    private long lastMessageTime
     private MechanizeAgent agent
+    private final net.zomis.duga.chat.listen.ListenTask domainTask;
     ScheduledFuture<?> future
 
     public ListenTask(DugaBotService bot, String room, ChatCommands commandHandler, DugaChatListener bean) {
@@ -46,6 +40,8 @@ class ListenTask implements Runnable {
         this.handler = commandHandler
         this.agent = new MechanizeAgent()
         this.commandPrefix = bean.environment.getProperty('commandPrefix', '@Duga ')
+        this.domainTask = new net.zomis.duga.chat.listen.ListenTask(bot, new StackExchangeFetch({bot.fkey()}), room,
+            {this.handle(it)})
 
         Binding binding = new Binding()
         CompilerConfiguration cc = new CompilerConfiguration()
@@ -115,67 +111,24 @@ class ListenTask implements Runnable {
         this.groovyShell = new GroovyShell(getClass().getClassLoader(), binding, cc)
     }
 
+    private void handle(ChatMessageIncoming message) {
+        def content = message.content
+        if (!content.startsWith(commandPrefix)) {
+            return
+        }
+        if (!authorizedCommander(message)) {
+            return
+        }
+        message.cleanHTML()
+        println "possible command: $message.content"
+        def commandResult = botCommand(message)
+        if (commandResult != null) {
+            message.reply('Result: ' + String.valueOf(commandResult))
+        }
+    }
+
     synchronized void latestMessages() {
-        Map<String, String> parameters = new HashMap<>();
-        String fkey = bot.fkey()
-        if (fkey == null) {
-            return
-        }
-        parameters.put("fkey", fkey);
-        parameters.put("mode", "messages");
-        parameters.put("msgCount", String.valueOf(NUM_MESSAGES));
-        Resource response = agent.post("http://chat.stackexchange.com/chats/" + room + "/events", parameters)
-
-        if (!(response instanceof JsonDocument)) {
-            println 'Unexpected response: ' + response
-            return
-        }
-
-        println 'Checking for events in room ' + room
-        def jsonDocument = response as JsonDocument
-        JsonNode node = jsonDocument.root
-        def json = new JsonSlurper().parseText(node.toString())
-        def events = json.events
-        long previousId = lastHandledId
-        for (def event in events) {
-            ChatMessageIncoming message = new ChatMessageIncoming(event as Map)
-            message.bot = bot
-            message.params = params
-            if (message.message_id <= lastHandledId) {
-                continue
-            }
-            lastHandledId = Math.max(lastHandledId, message.message_id)
-            lastMessageTime = Math.max(lastMessageTime, message.time_stamp)
-            if (previousId <= 0) {
-                println 'Previous id 0, skipping ' + message.content
-                continue
-            }
-            def content = message.content
-            if (!content.startsWith(commandPrefix)) {
-                continue
-            }
-            if (!authorizedCommander(message)) {
-                continue
-            }
-            message.cleanHTML()
-            println "possible command: $message.content"
-            def commandResult = botCommand(message)
-            if (commandResult != null) {
-                message.reply('Result: ' + String.valueOf(commandResult))
-            }
-//            handler.botCommand(message)
-        }
-        if (previousId <= 0) {
-            bot.postAsync(params.message('Monking! (Duga is now listening for commands)'))
-        }
-/*            Root node: {"ms":4,"time":41194973,"sync":1433551091,"events":
-                [{"room_id":16134,"event_type":1,"time_stamp":1433547911,"user_id":125580,"user_name":"Duga","message_id":22039309,"content":"Loki Astari vs. Simon Andr&#233; Forsberg: 4383 diff. Year: -1368. Quarter: -69. Month: -5. Week: +60. Day: -25."}
-                 ,{"room_id":16134,"event_type":1,"time_stamp":1433548817,"user_id":125580,"user_name":"Duga","message_id":22039366,"content":"<b><i>RELOAD!<\/i><\/b>"}
-                 ,{"room_id":16134,"event_type":1,"time_stamp":1433548849,"user_id":125580,"user_name":"Duga","message_id":22039371,"content":"&#91;<a href=\"https://github.com/retailcoder/Rubberduck\" rel=\"nofollow\"><b>retailcoder/Rubberduck<\/b><\/a>&#93; 12 commits. 2 closed issues. 5 issue comments."}
-                 ,{"room_id":16134,"event_type":1,"time_stamp":1433551821,"user_id":98071,"user_name":"Simon André Forsberg","parent_id":22039371,"show_parent":true,"message_id":22039768,"content":"@Duga No open issues today?"}]}
-
-        Success: {"id":22039802,"time":1433552063}
-*/
+        domainTask.run()
     }
 
     def botCommand(ChatMessageIncoming chatMessageIncoming) {
