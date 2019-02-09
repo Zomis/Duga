2151e00396442e7e8949f7652c28c3afa7100740
Simon Forsberg <zomis2k@hotmail.com>
2019-02-09 19:12:49 +0100

    Disable ratingdiff task (I give up. Mug wins)

--- grails-app/services/net/zomis/duga/DugaFileConfig.groovy
+++ grails-app/services/net/zomis/duga/DugaFileConfig.groovy
@@ -12,7 +12,7 @@ class DugaFileConfig {
         new TaskData("0 0 * * * *", "github"),
         new TaskData("0 * * * * *", "comments"),
         new TaskData("4 0 0 * * *", "unanswered;8595;codereview;***RELOAD!*** There are %unanswered% unanswered questions (%percentage%% answered)"),
-        new TaskData("0 45 23 * * *", "ratingdiff;16134;31562,23788;codereview"),
+//        new TaskData("0 45 23 * * *", "ratingdiff;16134;31562,23788;codereview"),
         new TaskData("0 0 */2 * * *", "mess;20298;The time is %time% and @Duga is alive"),
         new TaskData("0 */5 * * * *", "questionScan;codereview;answerInvalidation;8595")
     )
