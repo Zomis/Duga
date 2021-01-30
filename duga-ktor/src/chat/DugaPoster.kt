5dabe70071fd26408b772eb7cebbe893a563c474
Simon Forsberg <zomis2k@hotmail.com>
2021-01-30 16:26:40 +0100

    Add check for "You can perform this action again in"

--- duga-ktor/src/chat/DugaPoster.kt
+++ duga-ktor/src/chat/DugaPoster.kt
@@ -1,9 +1,11 @@
 package net.zomis.duga.chat
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.http.*
+import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 
 class DugaPoster(val duga: DugaBot) {
@@ -13,21 +15,34 @@ class DugaPoster(val duga: DugaBot) {
 
     data class PostResult(val id: Long, val time: Long)
     private suspend fun internalPost(room: String, message: String): PostResult? {
-        val result = duga.httpClient.post<String>(duga.chatUrl + "/chats/$room/messages/new") {
-            body = FormDataContent(Parameters.build {
-                append("text", message)
-                append("fkey", duga.fkey())
-            })
+        try {
+            val result = duga.httpClient.post<String>(duga.chatUrl + "/chats/$room/messages/new") {
+                body = FormDataContent(Parameters.build {
+                    append("text", message)
+                    append("fkey", duga.fkey())
+                })
+            }
+            logger.info("Post result: $result")
+            return mapper.readValue(result, PostResult::class.java)
+        } catch (e: ClientRequestException) {
+            if (e.message?.contains("You can perform this action again in") == true) {
+                val performAgainIn = e.message!!.substringAfter("You can perform this action again in").trim()
+                logger.info("perform again in $performAgainIn")
+                if (performAgainIn.substringAfter(' ').contains("second")) {
+                    delay(performAgainIn.substringBefore(' ').toLong() * 1000)
+                    return internalPost(room, message)
+                }
+            }
+            throw e
         }
-        logger.info("Post result: $result")
-        return mapper.readValue(result, PostResult::class.java)
     }
 
     suspend fun postMessage(room: String, message: String): PostResult? {
         return try {
             internalPost(room, message)
         } catch (e: Exception) {
-            logger.warn("Unable to post '$message' to room $room. Retrying", e)
+            logger.warn("Unable to post '$message' to room $room. Retrying in a little while", e)
+            delay(10_000L)
             duga.refreshFKey()
             try {
                 internalPost(room, message)
