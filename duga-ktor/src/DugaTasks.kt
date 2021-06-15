6f061b89dca4d31ec1721672c93ceecfb76bfefc
Simon Forsberg <zomis2k@hotmail.com>
2021-06-15 23:05:23 +0200

    Catch exception on initializing machine learning

--- duga-ktor/src/DugaTasks.kt
+++ duga-ktor/src/DugaTasks.kt
@@ -6,17 +6,24 @@ import net.zomis.duga.utils.stackexchange.CommentsScanTask
 import net.zomis.duga.utils.stackexchange.ProgrammersClassification
 import net.zomis.duga.utils.stackexchange.QuestionScanTask
 import net.zomis.machlearn.text.TextClassification
+import org.slf4j.LoggerFactory
 
-class DugaTasks(private val poster: DugaPoster, private val stackApi: StackExchangeApi) {
+class DugaTasks(private poster: DugaPoster, private stackApi: StackExchangeApi) {
     private val questionScanTask = QuestionScanTask(poster, stackApi, "codereview")
     private val programmersClassification: TextClassification
     private val commentsScanTask: CommentsScanTask
 
     init {
-        val trainingData = this::class.java.classLoader.getResource("trainingset-programmers-comments.txt")
-        val source = trainingData?.readText()
-        val lines = source?.split("\n")
-        this.programmersClassification = ProgrammersClassification.machineLearning(lines)
+        val programmers = try {
+            val trainingData = this::class.java.classLoader.getResource("trainingset-programmers-comments.txt")
+            val source = trainingData?.readText()
+            val lines = source?.split("\n")
+            ProgrammersClassification.machineLearning(lines)
+        } catch (e: Exception) {
+            LoggerFactory.getLogger(DugaTasks::class.java).warn("Unable to load machine learning classification", e)
+            ProgrammersClassification.machineLearning(emptyList())
+        }
+        this.programmersClassification = programmers
         this.commentsScanTask = CommentsScanTask(stackApi, programmersClassification, poster)
     }
 
