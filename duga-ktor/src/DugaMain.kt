c817bac6e08e99b57bcd8947fb5487a53b91b207
Simon Forsberg <zomis2k@hotmail.com>
2021-07-06 18:00:12 +0200

    Remove imports

--- duga-ktor/src/DugaMain.kt
+++ duga-ktor/src/DugaMain.kt
@@ -1,12 +1,9 @@
 package net.zomis.duga
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.github.shyiko.skedule.Schedule
-import kotlinx.coroutines.runBlocking
 import net.zomis.duga.chat.*
 import net.zomis.duga.server.ArgumentsCheck
 import net.zomis.duga.server.DugaServer
-import net.zomis.duga.tasks.Tasks
 import net.zomis.duga.utils.github.GitHubApi
 import net.zomis.duga.utils.github.HookString
 import net.zomis.duga.utils.stackexchange.StackExchangeApi
@@ -15,8 +12,6 @@ import net.zomis.duga.utils.stats.DugaStatsInternalMap
 import net.zomis.duga.utils.stats.DugaStatsNoOp
 import org.slf4j.LoggerFactory
 import java.io.File
-import java.time.DayOfWeek
-import java.time.temporal.ChronoUnit
 
 object DugaMain {
     private val logger = LoggerFactory.getLogger(DugaMain::class.java)
