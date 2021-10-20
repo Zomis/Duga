6357b6e27cccdb4b7921e97fcb6b1b4b8093cf9a
Simon Forsberg <zomis2k@hotmail.com>
2021-10-20 12:31:14 +0200

    Add alternative way of updating QuestionScanTask lastCheck
    To avoid being dependent on a correct system clock

--- duga-ktor/src/utils/stackexchange/QuestionScanTask.kt
+++ duga-ktor/src/utils/stackexchange/QuestionScanTask.kt
@@ -1,6 +1,7 @@
 package net.zomis.duga.utils.stackexchange
 
 import net.zomis.duga.chat.DugaPoster
+import org.slf4j.LoggerFactory
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
@@ -9,12 +10,16 @@ class QuestionScanTask(val poster: DugaPoster, val stackApi: StackExchangeApi, v
     private val FILTER = "!DEQ-Ts0KBm6n14zYUs8UZUsw.yj0rZkhsEKF2rI4kBp*yOHv4z4"
     val LATEST_QUESTIONS = "questions?order=desc&sort=activity"
 
+    private var logger = LoggerFactory.getLogger(this::class.java)
     private var lastCheck: Instant = Instant.now().minus(1, ChronoUnit.MINUTES)
 
     suspend fun run() {
         val previousCheck = this.lastCheck
         this.lastCheck = Instant.now()
         val questions = stackApi.apiCall(LATEST_QUESTIONS, site, FILTER)
+        val t = questions?.get("items")?.map { it.get("creation_date")?.asLong() ?: 0 }?.maxOrNull() ?: 0
+        logger.info("lastCheck highest post time is {}, previous lastCheck is {}", t, lastCheck.epochSecond)
+        this.lastCheck = Instant.ofEpochSecond(maxOf(lastCheck.epochSecond, t))
         AnswerInvalidationCheck.perform(poster, questions, previousCheck, stackApi)
     }
 
