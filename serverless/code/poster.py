93a756b06cce144e96eed003b085eda88cd0d0e2
Simon Forsberg <zomis2k@hotmail.com>
2026-02-16 15:53:41 +0100

    AWS SQS poster: Only count the messages, as formatting changes content

--- serverless/code/poster.py
+++ serverless/code/poster.py
@@ -21,31 +21,33 @@ def lambda_handler(event, context):
         messages_by_room[int(room_id)].append(payload)
 
     for room_id, texts in messages_by_room.items():
-        logging.debug(f"Handling room {room_id}")
+        print(f"Handling room {room_id}")
         room = client.get_room(room_id)
         send_messages_to_room(room, texts, me)
+        print(f"Finished handling of room {room_id}")
 
     return {
         'statusCode': 204
     }
 
 def send_messages_to_room(room, texts, me, timeout=10):
-    pending = set(texts)
+    pending = len(texts)
     deadline = time.time() + timeout
 
     with room.new_messages() as stream:
         for text in texts:
-            logging.debug(f"Sending message {text}")
+            print(f"Sending message {text}")
             room.send_message(text)
 
         for msg in stream:
-            if msg.owner is me and msg.content in pending:
-                pending.remove(msg.content)
+            print(f"Received message: {msg.content}")
+            if msg.owner is me:
+                pending -= 1
 
-                if not pending:
+                if pending == 0:
                     return
 
             if time.time() > deadline:
                 raise TimeoutError(
-                    f"Timed out waiting for {len(pending)} messages"
+                    f"Timed out waiting for {pending} messages"
                 )
